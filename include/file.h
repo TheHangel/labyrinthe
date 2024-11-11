7808ab29490e15f2aa050d44abe9099563c4d409
TheHangel <105165180+TheHangel@users.noreply.github.com>
2024-11-11 01:38:36 +0100

    save & load maze in file

--- include/file.h
+++ include/file.h
@@ -15,5 +15,7 @@
 char* get_maze_path(char *maze_name);
 char* get_leaderboard_path(char *maze_name);
 char** list_saves_files(const char *directory, int *file_count, extension ext);
+int save_maze_to_file(const char *filename, maze *m);
+maze* load_maze_from_file(const char *filename);
 
 #endif

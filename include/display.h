0277d3d96c9fc312737ede59b2cb8767e8b2b288
TheHangel <105165180+TheHangel@users.noreply.github.com>
2024-11-12 00:32:40 +0100

    display func declaration

--- include/display.h
+++ include/display.h
@@ -12,6 +12,12 @@ int menu_selection(WINDOW* w, const char *options[], int n_options, int menu_sta
 void display_end_window(maze *m);
 void display_game(maze *m);
 void display_title(WINDOW *win);
+void draw_textbox(WINDOW *win, int width, const char *label, char *text);
+void draw_checkbox(WINDOW *win, int y, int x, int checked);
+void draw_button(WINDOW *win, int y, int x, char *label);
+void display_message_window(const char *message);
+maze *display_create_maze();
+maze *display_maze_selection(maze_selection_mode mode);
 void display_main_menu();
 
 #endif
